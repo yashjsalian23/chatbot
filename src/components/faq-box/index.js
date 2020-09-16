563f5670d40e5a4704f59d2e8cc0a884f532637d
yashjsalian23 <yashjsalian23@gmail.com>
2020-09-16 11:31:51 +0530

    added question mark

--- src/components/faq-box/index.js
+++ src/components/faq-box/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import questionMark from '../../assets/question-mark.svg';
+
 import "./index.css";
 
 const index = () => {
@@ -7,7 +9,10 @@ const index = () => {
         <React.Fragment>
             <div id="faq-layout">
                 <div id="faq-content">
-                    <p id="faq-title">Frequently Asked Questions</p>
+                    <p id="faq-title">
+                        Frequently Asked Questions
+                        <span style={{marginLeft: 7}}><img src={questionMark} alt="question mark" /></span>
+                    </p>
                 </div>
             </div>
         </React.Fragment>
