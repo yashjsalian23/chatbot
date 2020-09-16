4154091c2264ba72f632a872908e00eb597d7a3f
yashjsalian23 <yashjsalian23@gmail.com>
2020-09-16 11:38:33 +0530

    added question layout and text

--- src/components/faq-box/index.js
+++ src/components/faq-box/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import questionMark from '../../assets/question-mark.svg';
+import Question from './question/Question';
 
 import "./index.css";
 
@@ -13,6 +14,7 @@ const index = () => {
                         Frequently Asked Questions
                         <span style={{marginLeft: 7}}><img src={questionMark} alt="question mark" /></span>
                     </p>
+                    <Question />
                 </div>
             </div>
         </React.Fragment>
---
+++ src/components/faq-box/question/Question.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Question.css';
+
+const Question = (props) => {
+    return (
+        <div className="question-layout">
+            <div className="question-content">
+                <span className="question-text">Can I redeem my FB before the original term?</span>
+            </div>
+        </div>
+    );
+};
+
+export default Question;
